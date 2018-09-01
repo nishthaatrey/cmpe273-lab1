e313ee56ac9d2b195c2835788bcdd7a741c96e1c
Nishtha Atrey <33688691+nishthaatrey@users.noreply.github.com>
2018-08-31 19:17:11 -0700

    Add files via upload

---
+++ http_async_call.py
@@ -0,0 +1,17 @@
+import requests
+import threading
+import time
+
+def async_call():
+    r=requests.get('https://webhook.site/#/10c68a23-5b6a-4f00-938a-a74d8d4a653a')
+    print(r.headers['date'])
+   
+
+t1=threading.Thread(target=async_call)
+t2=threading.Thread(target=async_call)
+t3=threading.Thread(target=async_call)
+
+t1.start()
+t2.start()
+t3.start()
+
