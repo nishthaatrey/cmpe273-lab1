7f66260abf3986a5cd7b8f5b23457a4ba494235c
Nishtha Atrey <33688691+nishthaatrey@users.noreply.github.com>
2018-09-01 18:21:42 -0700

    Update http_async_call.py

--- http_async_call.py
+++ http_async_call.py
@@ -1,18 +1,19 @@
+#Making the necessary imports
 import requests
 import asyncio
 
-
+#Function for making http calls to webhook using asynchronous technique and returning the date parameter
 def webhook():
     r=loop1.run_in_executor(None,requests.get,'https://webhook.site/#/10c68a23-5b6a-4f00-938a-a74d8d4a653a')
     result= yield from r
     return result.headers['Date']
     
-
+#Function for printing the received dates
 async def main(routine):
     for futures in asyncio.as_completed(routine):
         print(await futures)
 
-
+#Generating event loops using asyncio library for asynchrounous jobs, running the loops and finally closing them
 routine=[webhook() for i in range (0,3)]
 loop = asyncio.get_event_loop()
 loop1 = asyncio.get_event_loop()
