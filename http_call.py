df7d10560f55189db9ca1fba5a63f1babb773785
Nishtha Atrey <33688691+nishthaatrey@users.noreply.github.com>
2018-09-01 18:14:44 -0700

    Update http_call.py

--- http_call.py
+++ http_call.py
@@ -1,5 +1,8 @@
-import requests
 
+#making necessry imports
+import requests                   
+
+#Loop for 3 synchronous http calls
 for n in range(0,3):
     r=requests.get('https://webhook.site/#/10c68a23-5b6a-4f00-938a-a74d8d4a653a')
     print(r.headers['date'])
